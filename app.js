0881dc2cc7fcec9d66e1ee839e89805d2e773c27
LinGill21 <42749226+LinGill21@users.noreply.github.com>
2020-04-07 09:28:34 -0500

    can not npm start

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = express();
+const mongoose = require('mongoose');
+
+const dev_db_url = 'mongodb+srv://dbUser:JRw:7ax<En[7RE3L@cluster0-ozywb.azure.mongodb.net/local_library?retryWrites=true&w=majority'
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
